79d256cdf27ef8abb387d3559670af68ee89abb3
JSingerNCCER <jsinger@nccer.org>
2026-02-05 14:17:02 -0500

    Added another comment

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -1,5 +1,6 @@
 # Shipping Cost Calculator
 # Here is a new update by JSingerNCCER
+# Here is another update by JSingerNCCER
 
 ## Input package weight and shipping rate
 weight = float(input("Enter the package weight in kilograms: "))
